3c33c14232f085016dcc3d57b60e4ee445a157b4
Degives <f.degives@students.ephec.be>
2021-03-29 15:07:38 +0200

    maj

--- projet/frontend/src/App.js
+++ projet/frontend/src/App.js
@@ -1,38 +1,26 @@
 import React from "react";
-<<<<<<< HEAD
 /*import Navbar from './Navbar/navbar';
 import background from './Navbar/images/fond.png';*/
-=======
-import Navbar from './Navbar/navbar';
-import background from './Navbar/images/cardbackground.jpg';
->>>>>>> 7ce7abda7a900a3f18ac0eb250b264edf1e1268c
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Jeu from "./pages/Jeu/Jeu";
 import Signin from "./pages/Signin/Signin";
 import Rules from "./pages/Rules/Rules";
 import {Redirect, Route, Switch} from 'react-router-dom';
-import HowTo from "./pages/howto/howto";
+import HowTo from "./pages/Howto/Howto";
 import Stats from "./pages/Profil/Stats";
 import Me from "./pages/Profil/Me";
 import Historique from "./pages/Profil/Historique";
 
 function App() {
   return (
-<<<<<<< HEAD
     /*<div style={{
       backgroundImage : `url(${background})`, height:1000,
       backgroundSize: 'cove',
-=======
-    <div style={{
-      backgroundImage : `url(${background})`, height: 1000, margin: 0,
-      backgroundSize: 'cover', backgroundRepeat: 'no-repeat',
->>>>>>> 7ce7abda7a900a3f18ac0eb250b264edf1e1268c
       }}>
 
       <Navbar />
       <Home />
-<<<<<<< HEAD
     </div>*/
     <Switch>
       <Route exact path="/" component={Home} />
@@ -46,10 +34,6 @@ function App() {
       <Route exact path="/howto" component={HowTo} />
       <Redirect to="/" />
     </Switch>
-=======
-    </div>
-
->>>>>>> 7ce7abda7a900a3f18ac0eb250b264edf1e1268c
   );
 }
 
