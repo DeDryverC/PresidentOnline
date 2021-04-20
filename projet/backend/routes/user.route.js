7422630dc73fa0fa744d21feb3834f1868b5344e
Andreas Bombaert <a.bombaert@students.ephec.be>
2021-04-20 14:00:41 +0200

    cette fois-ci on oublie pas de push statistics

--- projet/backend/routes/user.route.js
+++ projet/backend/routes/user.route.js
@@ -2,7 +2,9 @@ module.exports = (app) => {
     const rules = require("../controllers/rules.controller");
     const history = require("../controllers/history.controller");
     const signin = require ("../controllers/signin.controller");
-    
-    app.get("/getRules", rules.getRules)
-    app.get("/getHistory", history.getHistory)
+    const statistics = require("../controllers/statistics.controller");
+
+    app.get("/getRules", rules.getRules);
+    app.get("/getHistory", history.getHistory);
+    app.get("/getStatistics/:pseudo", statistics.getStatistics);
 };
